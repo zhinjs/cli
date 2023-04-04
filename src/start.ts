a4547eb7749a5c2d3d7baf4f0354035190d1704d
凉菜 <1659488338@qq.com>
2023-04-04 13:52:28 +0800

    start 修复

--- src/start.ts
+++ src/start.ts
@@ -4,10 +4,10 @@ import {spawn} from "child_process";
 export default function registerStartCommand(cli:CAC){
     cli.command('start','启动知音')
         .action(async ()=>{
-            const node = spawn('npm', ['run','start:zhin'], { stdio: 'inherit' });
-            node.stdout?.on('data', data => process.stdout.push(data));
-            node.stderr?.on('data', data => process.stderr.push(data));
-            process.stdin?.on('data', data => node.stdin.write(data));
-            node.on('close', code => console.log(`child process exited with code ${code}\n`));
+            const cp = spawn('npm', ['run','start:zhin'], { stdio: 'pipe' });
+            cp.stdout?.on('data', data => process.stdout.push(data));
+            cp.stderr?.on('data', data => process.stderr.push(data));
+            process.stdin?.on('data', data => cp.stdin?.write(data));
+            cp.on('close', code => console.log(`child process exited with code ${code}\n`));
         })
 }
